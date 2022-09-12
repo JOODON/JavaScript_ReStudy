89c81e3cf3c24bf651d5764dd7906b7d9cdd470f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-09-12 12:58:19 +0900

    자바스크립트 선택자 선택!

--- src/Variable.js
+++ src/Variable.js
@@ -15,6 +15,28 @@
 // document.write(newstr+"<br/>");
 // var newArr=fruits.reverse();//리버스 쓰는 위치부터 다 바뀜!
 // document.write(newArr);
-
-setTimeout("")
-//측정할 시간 시간이 되면 할일
+//------------------------------------------------------------------
+//Time
+// function overLine(){
+//     alert("준비 되었나요?")
+// }
+// var timer=setTimeout(overLine,3000);
+// //측정할 시간 시간이 되면 할일,시간(시간은 1초당 1000으로 계산
+// clearTimeout(timer);
+// //클리어를 쓰면 아무것도 안됨
+// var timer2=setInterval(overLine,3000);
+////시간바다 반복할 동작!
+// clearTimeout(timer2);
+//------------------------------------------------------------------
+//선택자 선정!
+//ID타이틀 가져오기!
+var firstTitle=document.getElementById('title');
+console.log(firstTitle);
+//Class 가져오기
+var myList=document.getElementsByClassName("list");
+console.log(myList);
+//태그명으로 가져오기!
+var paragraphs=document.getElementsByTagName('p');
+console.log(paragraphs);
+//css 선택자로 가져오기
+var mylist2=document.querySelector(".List li")
